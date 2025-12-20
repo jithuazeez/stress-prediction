1ef86787ef5f8cbe8f86a616bc995287a8b90c67
jithu azeez <jithuazeez@jithus-MacBook-Air-og.local>
2025-12-20 16:44:16 +0000

    moment changes

--- experiments/shared/alignment.py
+++ experiments/shared/alignment.py
@@ -29,7 +29,9 @@ HR_DATA_PATHS = [
     Path("../reports/hr_1hz_cleaned.csv"),
      # Kaggle paths (if you upload the HR data)
     # Path("/kaggle/input/hr-data-ppg/hr_1hz_from_ppg.csv"),
-    Path("/kaggle/input/hr-1hz-ppg/hr_1hz_from_ppg.csv")
+    Path("/kaggle/input/hr-1hz-ppg/hr_1hz_from_ppg.csv"),
+    Path("/kaggle/input/hr-data-ppg/hr_1hz_from_ppg.csv")
+    
 ]
 
 # Cache for HR data to avoid reloading
