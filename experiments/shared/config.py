85afe447605d8a3dabfdf6c00c7c1746ec2ef6e3
jithu azeez <jithuazeez@jithus-MacBook-Air-og.local>
2025-12-20 16:40:45 +0000

    moment changes

--- experiments/shared/config.py
+++ experiments/shared/config.py
@@ -19,8 +19,8 @@ class Config:
     
     # Paths
     # data_path: Path = Path("/Users/jithuazeez/Documents/Msc/Dissertation/Datasets/VitaStress/data")
-    # data_path: Path = Path("/kaggle/input/vitastress/VitaStress/data")
-    data_path: Path = Path("/kaggle/input/vitastess2/VitaStress/data")
+    data_path: Path = Path("/kaggle/input/vitastress/VitaStress/data")
+    # data_path: Path = Path("/kaggle/input/vitastess2/VitaStress/data")
     # results_base_path: Path = Path("/Users/jithuazeez/Documents/Msc/Dissertation/experiments")
     results_base_path: Path = Path("/kaggle/working/stress-prediction/experiments")
     # Window parameters
